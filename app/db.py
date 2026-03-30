e6294540438ae554c407aaab6ec62ac5692de113
nikhil.sharma <nikhil.sharma@calabrio.com>
2026-03-30 14:45:43 +0530

    Added DB file

---
+++ app/db.py
@@ -0,0 +1,7 @@
+# app/db.py
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import MONGO_URL, DB_NAME
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client[DB_NAME]
+collection = db["documents"]
